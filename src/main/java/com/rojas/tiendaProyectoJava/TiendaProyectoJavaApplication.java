d40e6cd173bb076b2fe31dab31a07c0d694e92d5
Soledad Rojas <gabuzitasol@gmail.com>
2024-04-18 18:13:46 -0300

    completar carga de proyecto

---
+++ src/main/java/com/rojas/tiendaProyectoJava/TiendaProyectoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rojas.tiendaProyectoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaProyectoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaProyectoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rojas/tiendaProyectoJava/TiendaProyectoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojas.tiendaProyectoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaProyectoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
